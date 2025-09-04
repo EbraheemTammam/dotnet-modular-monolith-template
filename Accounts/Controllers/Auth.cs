d064170a7d4337fb675a44aa55fc978e2cbe7b9f
Ebraheem Tammam <ebraheemtammam@gmail.com>
2025-09-04 11:04:26 +0300

    [FIX] add auth

--- Accounts/Controllers/Auth.cs
+++ Accounts/Controllers/Auth.cs
@@ -1,5 +1,4 @@
 using System.ComponentModel.DataAnnotations;
-using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 
--- Accounts/Utilities/ModuleRegistrar.cs
+++ Accounts/Utilities/ModuleRegistrar.cs
@@ -33,22 +33,26 @@ internal class AccountsModuleRegistrar : IModuleRegistrar
         .AddDefaultTokenProviders();
 
         services.AddAuthentication();
-        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = bool.Parse(Environment.GetEnvironmentVariable("JWT_VALIDATE_ISSUER")!),
-                        ValidateAudience = bool.Parse(Environment.GetEnvironmentVariable("JWT_VALIDATE_AUDIENCE")!),
-                        ValidateLifetime = bool.Parse(Environment.GetEnvironmentVariable("JWT_VALIDATE_LIFETIME")!),
-                        ValidateIssuerSigningKey = bool.Parse(Environment.GetEnvironmentVariable("JWT_VALIDATE_ISSUER_SIGNING_KEY")!),
-                        ValidIssuer = Environment.GetEnvironmentVariable("JWT_VALID_ISSUER"),
-                        ValidAudience = Environment.GetEnvironmentVariable("JWT_VALID_AUDIENCE"),
-                        IssuerSigningKey = new SymmetricSecurityKey(
-                            Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("SECRET_KEY")!)
-                        )
-                    };
-                });
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = bool.Parse(Environment.GetEnvironmentVariable("JWT_VALIDATE_ISSUER")!),
+                ValidateAudience = bool.Parse(Environment.GetEnvironmentVariable("JWT_VALIDATE_AUDIENCE")!),
+                ValidateLifetime = bool.Parse(Environment.GetEnvironmentVariable("JWT_VALIDATE_LIFETIME")!),
+                ValidateIssuerSigningKey = bool.Parse(Environment.GetEnvironmentVariable("JWT_VALIDATE_ISSUER_SIGNING_KEY")!),
+                ValidIssuer = Environment.GetEnvironmentVariable("JWT_VALID_ISSUER"),
+                ValidAudience = Environment.GetEnvironmentVariable("JWT_VALID_AUDIENCE"),
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("SECRET_KEY")!)
+                )
+            };
+        });
         services.AddAuthorization();
 
         services.AddScoped<CreateSuperUserCommand>();
